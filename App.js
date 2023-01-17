45267bb0cdd968f480ac1234bf3857ad29e7da50
Adrian1447 <antrip_147@live.com>
2023-01-16 22:13:18 -0500

    xd

--- App.js
+++ App.js
@@ -1,46 +1,54 @@
-import React, { useState } from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { AppBar, HStack, IconButton, Backdrop, BackdropSubheader, Button} from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import {
+  AppBar,
+  Backdrop,
+  BackdropSubheader,
+  Button,
+  IconButton,
+} from "@react-native-material/core";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   const [revealed, setRevealed] = useState(false);
   return (
-    <Backdrop
-      revealed={revealed}
-      header={
-    <AppBar
-    title="Title" 
-    transparent
-    leading={props => (
-      <IconButton
-        icon={props => (
-          <Icon name={revealed ? "close" : "menu"} {...props} />
-        )}
-        onPress={() => setRevealed(prevState => !prevState)}
-        {...props}
-      />
-    )}
-  />
-  }
-  backLayer={<View style={{ height: 120 }} />}
-  >
-  <BackdropSubheader title="Subheader" />
-  <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Click Me" onPress={() => alert("🎉🎉🎉")}/>
-      <StatusBar style="auto" />
-    </View>
-  </Backdrop>
+    <NavigationContainer>
+      <Backdrop
+        revealed={revealed}
+        header={
+          <AppBar
+            title="Title"
+            transparent
+            leading={(props) => (
+              <IconButton
+                icon={(props) => (
+                  <Icon name={revealed ? "close" : "menu"} {...props} />
+                )}
+                onPress={() => setRevealed((prevState) => !prevState)}
+                {...props}
+              />
+            )}
+          />
+        }
+        backLayer={<View style={{ height: 120 }} />}
+      >
+        <BackdropSubheader title="Subheader" />
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Button title="Click Me" onPress={() => alert("🎉🎉🎉")} />
+          <StatusBar style="auto" />
+        </View>
+      </Backdrop>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 30,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
