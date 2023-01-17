b0e336fab87f3dcc0bec0a7c903d946787043a96
Adrian1447 <antrip_147@live.com>
2023-01-16 22:24:27 -0500

    xd

--- App.js
+++ App.js
@@ -1,54 +1,19 @@
-import Icon from "@expo/vector-icons/MaterialCommunityIcons";
-import {
-  AppBar,
-  Backdrop,
-  BackdropSubheader,
-  Button,
-  IconButton,
-} from "@react-native-material/core";
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import * as React from "react";
+import HomeScreen from "./Home";
+import ProfileScreen from "./ProfileScreen";
 
 export default function App() {
-  const [revealed, setRevealed] = useState(false);
   return (
     <NavigationContainer>
-      <Backdrop
-        revealed={revealed}
-        header={
-          <AppBar
-            title="Title"
-            transparent
-            leading={(props) => (
-              <IconButton
-                icon={(props) => (
-                  <Icon name={revealed ? "close" : "menu"} {...props} />
-                )}
-                onPress={() => setRevealed((prevState) => !prevState)}
-                {...props}
-              />
-            )}
-          />
-        }
-        backLayer={<View style={{ height: 120 }} />}
-      >
-        <BackdropSubheader title="Subheader" />
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Button title="Click Me" onPress={() => alert("🎉🎉🎉")} />
-          <StatusBar style="auto" />
-        </View>
-      </Backdrop>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 30,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
